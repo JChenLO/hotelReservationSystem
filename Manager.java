786bb64ee9e95b4f5bf91ec1c9d45cc0facfd3d0
Animak3000 <cisspook@aim.com>
2014-11-07 16:18:31 -0800

    Create Manager.java

---
+++ Manager.java
@@ -0,0 +1,16 @@
+/**
+ * A Manager Account
+ * @author Andre Mak
+ */
+public class Manager extends User
+{
+    public Manager(int id, String name, String password)
+    {
+        super(id, name, password);
+    }
+    
+    public void checkReservation(String startDate)
+    {
+        //list.getReservationByDate(startDate);
+    }
+}
