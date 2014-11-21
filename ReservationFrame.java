fed89931c38210fa274ae6c28e47a0772df2c8ac
proteing <jchen918@gmail.com>
2014-11-20 23:47:10 -0800

    add make reservation method in ReservationFrame.java

--- ReservationFrame.java
+++ ReservationFrame.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.Iterator;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -25,12 +27,16 @@ public class ReservationFrame
    private JTextField checkoutField;
    private ButtonGroup group;
    private final SimpleDateFormat dt = new SimpleDateFormat("MM/dd/yyyy");
+   private JList roomList;
+   private final JFrame frame = new JFrame();
+   Calendar inDate = Calendar.getInstance();
+   Calendar outDate= Calendar.getInstance();  
    
        ReservationFrame(Hotel h, Guest g)
        {
           hotel = h;
            guest = g;
-           final JFrame frame = new JFrame();
+
            frame.setTitle("MaGeC Hotel Reservation Interface");
            frame.setSize(600,600);
            
@@ -58,7 +64,7 @@ public class ReservationFrame
            JPanel northSouthPanel = new JPanel();
            JLabel roomTypeLabel = new JLabel("Room Type:");
            JRadioButton luxButton = new JRadioButton("$200");
-           JRadioButton ecoButton = new JRadioButton("$100");
+           JRadioButton ecoButton = new JRadioButton("$80");
            group = new ButtonGroup();
            group.add(luxButton);
            group.add(ecoButton);
@@ -77,14 +83,16 @@ public class ReservationFrame
 
            
            //View
-           JPanel centerPanel = new JPanel();
-           JTextArea roomTextArea = new JTextArea();
-           roomTextArea.setSize(centerPanel.getSize());
-           centerPanel.add(roomTextArea);
-
-           String rooms = getAvailableRooms();
-           roomTextArea.setText(rooms);
-           
+           final JPanel centerPanel = new JPanel();
+           final JLabel confirmLabel = new JLabel();
+           centerPanel.setLayout(new BorderLayout());
+           setRoomView();
+           centerPanel.add(roomList, BorderLayout.CENTER);
+           centerPanel.add(confirmLabel, BorderLayout.SOUTH);
+           Border lineBorder2 = BorderFactory.createLineBorder(Color.GRAY);
+           centerPanel.setBorder(lineBorder2);
+           centerPanel.setBackground(Color.WHITE);
+       
            //buttons
            JPanel bottomPanel = new JPanel();
            JButton confirmButton = new JButton("Confirm");
@@ -93,7 +101,11 @@ public class ReservationFrame
                public void actionPerformed(ActionEvent e)
                {
                    /*make reservations */
+                   Room room = (Room) roomList.getSelectedValue();
                    
+                   Reservation r = new Reservation(inDate.getTime(), outDate.getTime(), guest.getID());
+                   room.addReservation(r);
+                   confirmLabel.setText("made reservation on " + room.toString());
                }
            });
            JButton transactionDoneButton = new JButton("Transaction Done");
@@ -102,7 +114,7 @@ public class ReservationFrame
                public void actionPerformed(ActionEvent e)
                {
                    /* done dispose frame */
-                   
+                   frame.dispose();
                }
            });
            bottomPanel.add(confirmButton);
@@ -113,14 +125,15 @@ public class ReservationFrame
            frame.add(centerPanel, BorderLayout.CENTER);
            frame.add(bottomPanel, BorderLayout.SOUTH);
            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+          // frame.pack();
            frame.setVisible(true);
        }
        
        
-       private String getAvailableRooms()
+       private void setRoomView()
        {
-          Calendar inDate = Calendar.getInstance();
-          Calendar outDate= Calendar.getInstance();     
+          
+          ArrayList<Room> rooms;
           try
         {
            inDate.setTime(dt.parse(checkinField.getText()));
@@ -134,10 +147,14 @@ public class ReservationFrame
              AbstractButton b = bs.nextElement();
              if(b.isSelected()){
                 String roomType = b.getText().substring(1);
-                return hotel.getAvailableRooms(inDate, outDate, Integer.parseInt(roomType));
+                rooms = hotel.getAvailableRooms(inDate, outDate, Integer.parseInt(roomType));
+                
+                
+                   roomList = new JList(rooms.toArray());
+                   if(roomList.getModel().getSize() > 0)roomList.setSelectedIndex(0);
              }
           }
-          return "No Room Available, please change date or room type and try again";
+          
        }
        
    }
