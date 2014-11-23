c570f35b3da1b26e4c1b1c4db5b7658d8e6b8741
proteing <jchen918@gmail.com>
2014-11-22 22:09:05 -0800

    Reservation Frame button function updated

--- ReservationFrame.java
+++ ReservationFrame.java
@@ -18,6 +18,8 @@ import java.util.Iterator;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class ReservationFrame
 {
@@ -27,136 +29,135 @@ public class ReservationFrame
    private JTextField checkoutField;
    private ButtonGroup group;
    private final SimpleDateFormat dt = new SimpleDateFormat("MM/dd/yyyy");
-   private JList roomList;
    private final JFrame frame = new JFrame();
    Calendar inDate = Calendar.getInstance();
    Calendar outDate= Calendar.getInstance();  
-   
-       ReservationFrame(Hotel h, Guest g)
-       {
-          hotel = h;
-           guest = g;
+   private final JPanel northPanel = new JPanel();
+   private final JPanel centerPanel = new JPanel();
+   private final JPanel bottomPanel = new JPanel();
+   private int total = 0;
+   ReservationFrame(Hotel h, Guest g)
+   {
+      hotel = h;
+      guest = g;
 
-           frame.setTitle("MaGeC Hotel Reservation Interface");
-           frame.setSize(600,600);
-           
-           //Control - may need to factorize to seperate panel
-           JPanel northPanel = new JPanel();
-           northPanel.setLayout(new BorderLayout());
-           
-           JPanel northCenterPanel = new JPanel();
-           northCenterPanel.setLayout(new GridLayout(2,2));
-           
-           JLabel checkinLabel = new JLabel("Check-in:");
-           JLabel checkoutLabel = new JLabel("Check-out:");
-           
-           Calendar cal = Calendar.getInstance();
+      frame.setTitle("MaGeC Hotel Reservation Interface");
+      frame.setSize(600,600);
 
-           checkinField = new JTextField(dt.format(cal.getTime()));
-           cal.add(Calendar.DATE, 7); 
-           checkoutField = new JTextField(dt.format(cal.getTime()));
-           
-           northCenterPanel.add(checkinLabel);
-           northCenterPanel.add(checkoutLabel);
-           northCenterPanel.add(checkinField);
-           northCenterPanel.add(checkoutField);
-           
-           JPanel northSouthPanel = new JPanel();
-           JLabel roomTypeLabel = new JLabel("Room Type:");
-           JRadioButton luxButton = new JRadioButton("$200");
-           JRadioButton ecoButton = new JRadioButton("$80");
-           group = new ButtonGroup();
-           group.add(luxButton);
-           group.add(ecoButton);
-           group.setSelected(luxButton.getModel(), true);
-          
-           northSouthPanel.add(roomTypeLabel);
-           northSouthPanel.add(luxButton);
-           northSouthPanel.add(ecoButton);
-           
-           northPanel.add(northCenterPanel, BorderLayout.CENTER);
+      //Checkin checkout date
+      northPanel.setLayout(new BorderLayout());
 
-           Border lineBorder1 = BorderFactory.createLineBorder(Color.GRAY);
-           northPanel.setBorder(lineBorder1);
-           
-           northPanel.add(northSouthPanel, BorderLayout.SOUTH);
+      JPanel northCenterPanel = new JPanel();
+      northCenterPanel.setLayout(new GridLayout(2,2));
 
-           
-           //View
-           final JPanel centerPanel = new JPanel();
-           final JLabel confirmLabel = new JLabel();
-           centerPanel.setLayout(new BorderLayout());
-           setRoomView();
-           centerPanel.add(roomList, BorderLayout.CENTER);
-           centerPanel.add(confirmLabel, BorderLayout.SOUTH);
-           Border lineBorder2 = BorderFactory.createLineBorder(Color.GRAY);
-           centerPanel.setBorder(lineBorder2);
-           centerPanel.setBackground(Color.WHITE);
-       
-           //buttons
-           JPanel bottomPanel = new JPanel();
-           JButton confirmButton = new JButton("Confirm");
-           confirmButton.addActionListener(new ActionListener()
-           {
-               public void actionPerformed(ActionEvent e)
-               {
-                   /*make reservations */
-                   Room room = (Room) roomList.getSelectedValue();
-                   
-                   Reservation r = new Reservation(inDate.getTime(), outDate.getTime(), guest.getID());
-                   room.addReservation(r);
-                   confirmLabel.setText("made reservation on " + room.toString());
-               }
-           });
-           JButton transactionDoneButton = new JButton("Transaction Done");
-           transactionDoneButton.addActionListener(new ActionListener()
-           {
-               public void actionPerformed(ActionEvent e)
-               {
-                   /* done dispose frame */
-                   frame.dispose();
-               }
-           });
-           bottomPanel.add(confirmButton);
-           bottomPanel.add(transactionDoneButton);
-           
-           
-           frame.add(northPanel, BorderLayout.NORTH);
-           frame.add(centerPanel, BorderLayout.CENTER);
-           frame.add(bottomPanel, BorderLayout.SOUTH);
-           frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
-          // frame.pack();
-           frame.setVisible(true);
-       }
-       
-       
-       private void setRoomView()
-       {
-          
-          ArrayList<Room> rooms;
-          try
-        {
-           inDate.setTime(dt.parse(checkinField.getText()));
-           outDate.setTime(dt.parse(checkinField.getText()));
-        } catch (ParseException e1)
-        {
-           // TODO Auto-generated catch block
-           e1.printStackTrace();
-        }
-          for(Enumeration<AbstractButton> bs = group.getElements() ; bs.hasMoreElements();){
-             AbstractButton b = bs.nextElement();
-             if(b.isSelected()){
-                String roomType = b.getText().substring(1);
-                rooms = hotel.getAvailableRooms(inDate, outDate, Integer.parseInt(roomType));
-                
-                
-                   roomList = new JList(rooms.toArray());
-                   if(roomList.getModel().getSize() > 0)roomList.setSelectedIndex(0);
-             }
-          }
-          
-       }
-       
+      JLabel checkinLabel = new JLabel("Check-in:");
+      JLabel checkoutLabel = new JLabel("Check-out:");
+
+      Calendar cal = Calendar.getInstance();
+      checkinField = new JTextField(dt.format(cal.getTime()));
+      cal.add(Calendar.DATE, 7); 
+      checkoutField = new JTextField(dt.format(cal.getTime()));
+
+      northCenterPanel.add(checkinLabel);
+      northCenterPanel.add(checkoutLabel);
+      northCenterPanel.add(checkinField);
+      northCenterPanel.add(checkoutField);
+
+      final JPanel northSouthPanel = new JPanel();
+      JLabel roomTypeLabel = new JLabel("Room Type:");
+      final JRadioButton luxButton = new JRadioButton("$200");
+      final JRadioButton ecoButton = new JRadioButton("$80");
+      group = new ButtonGroup();
+      group.add(luxButton);
+      group.add(ecoButton);
+      group.setSelected(luxButton.getModel(), true);
+
+      JButton showButton = new JButton("Show Available Rooms");
+      showButton.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            try
+            {
+               inDate.setTime(dt.parse(checkinField.getText()));
+               outDate.setTime(dt.parse(checkinField.getText()));
+            } catch (ParseException e1)
+            {
+               // TODO Auto-generated catch block
+               e1.printStackTrace();
+            }
+            int roomType = luxButton.isSelected() ? 200 : 80;
+            hotel.setAvailableRooms(inDate, outDate, roomType);
+         }
+      });
+
+      northSouthPanel.add(roomTypeLabel);
+      northSouthPanel.add(luxButton);
+      northSouthPanel.add(ecoButton);
+      northSouthPanel.add(showButton);
+      northPanel.add(northCenterPanel, BorderLayout.CENTER);
+      Border lineBorder1 = BorderFactory.createLineBorder(Color.GRAY);
+      northPanel.setBorder(lineBorder1);
+      northPanel.add(northSouthPanel, BorderLayout.SOUTH);
+
+
+      //View
+      centerPanel.setLayout(new BorderLayout());           
+      final JList<Room> jlist = new JList<Room>();
+      hotel.attach(new ChangeListener(){
+         public void stateChanged(ChangeEvent e){
+            DefaultListModel<Room> model = new DefaultListModel<Room>();
+            ArrayList<Room> rooms = hotel.getAvailableRooms();
+            for(Room r : rooms){
+               model.addElement(r);
+            }
+            //notify method
+            jlist.setModel(model);
+            if(model.size() > 0)jlist.setSelectedIndex(0);
+         }
+      });
+
+      centerPanel.add(jlist);
+      final JLabel confirmLabel = new JLabel();
+      centerPanel.add(confirmLabel, BorderLayout.SOUTH);
+      Border lineBorder2 = BorderFactory.createLineBorder(Color.GRAY);
+      centerPanel.setBorder(lineBorder2);
+      centerPanel.setBackground(Color.WHITE);
+
+      //buttons
+      JButton confirmButton = new JButton("Confirm");
+      confirmButton.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            /*make reservations */
+            Room room = jlist.getSelectedValue();
+            Reservation r = new Reservation(inDate.getTime(), outDate.getTime(), guest.getID());
+            total += room.getCost();
+            room.addReservation(r);
+            confirmLabel.setText("made reservation on " + room.toString() + "  Your total = " + total);
+         }
+      });
+      JButton transactionDoneButton = new JButton("Transaction Done");
+      transactionDoneButton.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            /* done dispose frame */
+            frame.dispose();
+         }
+      });
+      bottomPanel.add(confirmButton);
+      bottomPanel.add(transactionDoneButton);
+
+
+      frame.add(northPanel, BorderLayout.NORTH);
+      frame.add(centerPanel, BorderLayout.CENTER);
+      frame.add(bottomPanel, BorderLayout.SOUTH);
+      frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+      frame.setVisible(true);
    }
+
+}
 
 
