0bf209b92bd08cab134c7d5114309fd631d2511a
proteing <jchen918@gmail.com>
2014-11-23 00:52:39 -0800

    display receipt, use strategy pattern

---
+++ PrintFrame.java
@@ -0,0 +1,88 @@
+/**
+ @author Jie Chen
+ */
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.SimpleFormatter;
+
+import javax.swing.*;
+import javax.swing.GroupLayout.Group;
+import javax.swing.border.Border;
+
+//context program for strategy pattern
+public class PrintFrame
+{
+   private Hotel hotel;
+   private Guest guest;
+   private int transactionID;
+    PrintFrame(Hotel h, Guest g, int id)
+    {
+       hotel = h;
+       guest = g;
+       transactionID = id;
+       
+        final JFrame frame = new JFrame();
+        frame.setTitle("MaGeC Hotel Print");
+        frame.setSize(600,400);
+        
+        //title
+        JPanel northPanel = new JPanel();
+        JLabel label = new JLabel("Hotel Account Print");
+        northPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+        label.setFont(new Font("Serif", Font.PLAIN, 24));
+        northPanel.add(label);
+
+
+        
+        //input new user information
+        JPanel centerPanel = new JPanel();
+        
+        JRadioButton simpleButton = new JRadioButton("Print Simple Receipt");
+        JRadioButton comprehensiveButton = new JRadioButton("Print Comprehensive Receipt");
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(simpleButton);
+        group.add(comprehensiveButton);
+        
+           
+        centerPanel.add(simpleButton);
+        centerPanel.add(comprehensiveButton);
+      
+        Border lineBorder1 = BorderFactory.createLineBorder(Color.GRAY);
+        centerPanel.setBorder(lineBorder1);
+        
+        //submit button
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        bottomPanel.add(textArea, BorderLayout.CENTER);
+        
+
+        simpleButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+               ReceiptFormatter r = new SimpleReceipt(hotel, guest, transactionID);
+                textArea.setText(r.format() );
+            }
+        });
+        
+        comprehensiveButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {  ReceiptFormatter r = new ComprehensiveReceipt(hotel, guest);
+                textArea.setText(r.format());
+            }
+        });
+        
+
+        //finished frame
+        frame.add(northPanel, BorderLayout.NORTH);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setVisible(true);
+   }
+}
